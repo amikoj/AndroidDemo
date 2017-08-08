5a3532e245e28804f2352b34233a30bb8fa3cd4c
caihaifei <caihaifei@haolianluo.com>
2017-08-08 17:28:34 +0800

    create ActionBarDemo project.

---
+++ ActionBarDemo/src/main/java/com/haolianluo/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.haolianluo.myapplication
+
+import android.app.ActionBar
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+
+
+}
