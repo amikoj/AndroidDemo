3aa5fce14cc8ac43c6e4959e2e193e5aadb70404
hfcai <caihaifei@enjoytoday.cn>
2017-08-09 06:13:43 +0800

    merged fileprovider project.

---
+++ FileProviderDemo/src/main/java/com/hfcai/fileprovider/TestFileProvider.java
@@ -0,0 +1,12 @@
+package com.hfcai.fileprovider;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * @date 17-1-21.
+ * @className TestFileProvider
+ * @serial 1.0.0
+ */
+
+public class TestFileProvider extends FileProvider {
+}
